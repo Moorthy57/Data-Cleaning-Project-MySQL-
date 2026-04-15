e5e2dcfe763d61b79a80d5dc821e70e3e81573b9
Moorthy57 <yadavesakkimoorthy@gmail.com>
2026-04-15 18:56:00 +0530

    Add files via upload

---
+++ Data Cleaning Project ( Alex The Analyst ).sql
@@ -0,0 +1,8 @@
+use `project cleaning data`; -- put backtick when dosent used unerscore between name of the data base 
+select * from layoffs;
+
+-- 1. Remove Dupliactes 
+-- 2. Standardize the Data
+-- 3. Null Vlaues or Blank Vlaues 
+-- 4. Remove Any Columns 
+
